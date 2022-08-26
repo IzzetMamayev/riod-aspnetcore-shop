467898db0276a9431da8ee79e5bc2870376324e3
Izzet Mamayev <izzetmamaev@gmail.com>
2022-08-26 20:19:30 +0200

    Applied ORM

---
+++ RiodShopCore/RiodShopCore/Data/RiodDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RiodShopCore.Entities;
+
+namespace RiodShopCore.Data
+{
+    public class RiodDbContext : DbContext
+    {
+        public RiodDbContext() : base()
+        {
+
+        }
+
+        public RiodDbContext(DbSet<Contact> contacts)
+        {
+            Contacts = contacts;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server = IZZET; Database = RiodWithGit; Trusted_Connection = True;");
+            }
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
